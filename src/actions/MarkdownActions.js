b8f166e3e5980ffaad4e30b60b1e598ae512998c
yoat <yoatnic@me.com>
2018-05-16 20:58:13 +0900

    add actions

--- src/actions/MarkdownActions.js
+++ src/actions/MarkdownActions.js
@@ -1,11 +1,19 @@
 //@flow
 export const MARKDOWN_EDIT = "MARKDOWN/EDIT";
+export const MARKDOWN_ADD = "MARKDOWN/ADD";
 
 export type MdEditAction = {
   type: string,
   payload: {
     markdown: string
   }
+};
+
+export const addMarkdown = (markdown: string): MdEditAction => {
+  return {
+    type: MARKDOWN_EDIT,
+    payload: { markdown }
+  };
 };
 
 export const editMarkdown = (markdown: string): MdEditAction => {
