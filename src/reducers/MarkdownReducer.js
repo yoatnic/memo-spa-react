8e18a2a6d310ea8b3beeb8e9ee6c951122c16758
yoat <yoatnic@me.com>
2018-05-17 21:10:07 +0900

    add reducer

--- src/reducers/MarkdownReducer.js
+++ src/reducers/MarkdownReducer.js
@@ -1,11 +1,22 @@
 //@flow
-import { MARKDOWN_EDIT } from "../actions/MarkdownActions";
+import { MARKDOWN_EDIT, MARKDOWN_ADD } from "../actions/MarkdownActions";
 
-export const markdownReducer = (state = {}, action) => {
+export const markdownReducer = (
+  state = {
+    markdowns: []
+  },
+  action
+) => {
   switch (action.type) {
   case MARKDOWN_EDIT:
+    const markdowns = [...state.markdowns];
+    markdowns[action.payload.itemId] = action.payload.markdown;
     return Object.assign({}, state, {
-      markdown: action.payload.lastItemId
+      markdowns: markdowns
+    });
+  case MARKDOWN_ADD:
+    return Object.assign({}, state, {
+      markdowns: [...state.markdowns, action.payload.markdown]
     });
   default:
     return state;
